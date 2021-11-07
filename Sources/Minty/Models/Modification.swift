a54129a73dc2714c2108c8fc1cb5f6feaf6f06d0
Gene Koval <genyaremchuk@protonmail.com>
2021-11-07 12:44:36 -0500

    Add non-decoding init for Modification

--- Sources/Minty/Models/Modification.swift
+++ Sources/Minty/Models/Modification.swift
@@ -5,6 +5,11 @@ public struct Modification<T: ZiplineCodable>: ZiplineCodable {
     public var modified: Date
     public var newValue: T
 
+    public init(newValue: T, modified: Date = Date()) {
+        self.newValue = newValue
+        self.modified = modified
+    }
+
     public init(from decoder: ZiplineDecoder) throws {
         modified = try Date(from: decoder)
         newValue = try T(from: decoder)

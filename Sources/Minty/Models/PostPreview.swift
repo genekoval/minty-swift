bd8c1df7213743cc0c0c03c8ab9ea07044ee2327
Gene Koval <genyaremchuk@protonmail.com>
2022-01-24 23:11:51 -0500

    Change PostPreview preview type to ObjectPreview

--- Sources/Minty/Models/PostPreview.swift
+++ Sources/Minty/Models/PostPreview.swift
@@ -4,7 +4,7 @@ import Zipline
 public struct PostPreview: Codable, Hashable, Identifiable, ZiplineObject {
     public var id: String = ""
     public var title: String?
-    public var previewId: String?
+    public var preview: ObjectPreview?
     public var commentCount: UInt32 = 0
     public var objectCount: UInt32 = 0
     public var dateCreated: Date = Date()
@@ -12,7 +12,7 @@ public struct PostPreview: Codable, Hashable, Identifiable, ZiplineObject {
     public var coders: [Coder<Self>] {[
         Coder(\Self.id),
         Coder(\Self.title),
-        Coder(\Self.previewId),
+        Coder(\Self.preview),
         Coder(\Self.commentCount),
         Coder(\Self.objectCount),
         Coder(\Self.dateCreated)

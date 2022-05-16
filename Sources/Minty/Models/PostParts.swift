f1498caf979f2dee58fb444a442603cefb5dd75a
Gene Koval <genyaremchuk@protonmail.com>
2022-05-16 03:36:50 -0400

    Update PostParts title/description types

--- Sources/Minty/Models/PostParts.swift
+++ Sources/Minty/Models/PostParts.swift
@@ -2,8 +2,8 @@ import Foundation
 import Zipline
 
 public struct PostParts: Codable, Hashable, ZiplineObject {
-    public var title: String?
-    public var description: String?
+    public var title: String
+    public var description: String
     public var objects: [UUID] = []
     public var tags: [UUID] = []
 
